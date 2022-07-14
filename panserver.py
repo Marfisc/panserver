c5f792da9f0b01eb4f1f54cff7eea94c6b54722b
Devon Rueckner <devon.rueckner@gmail.com>
2022-07-14 16:47:47 -0400

    support dark mode

--- panserver.py
+++ panserver.py
@@ -307,9 +307,10 @@ def route_index():
 
     text += "<html><head><title>Panserver Index</title>"
     text += '<style type="text/css">{}</style>'.format(style_basic + style_index)
-    text += "</head><body>"
+    text += "</head><body class='markdown-body' style='max-width: 600px; margin: auto; margin-top: 80px; padding: 80px;'>"
     text += '<h1>Panserver</h1>'
     text += 'Serving Markdown documents rendered using <a href="http://pandoc.org/">pandoc</a>. By <a href="http://marcelfischer.eu/">Marcel Fischer</a>'
+    text += markdown_css_link
 
     def dir_entry(dirname, toplevel = False):
         #collect markdown files recursively into a list
@@ -365,6 +366,10 @@ meta_no_mobilescale = """<meta name="viewport" content="width=device-width, init
 
 
 style_basic = """
+        html { background-color: #eee; }
+        @media (prefers-color-scheme: dark) {
+            html { background-color: #272727; }
+        }
         body {
             width: 92%; max-width: 40em;  margin: auto;
             font-size: 1.2rem; line-height: 1.5; padding-bottom: 3em;}
@@ -399,9 +404,17 @@ style_index = """
         ul li.file-entry{
             list-style-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAA00lEQVQ4jcXUQUqCURSG4Qc0bKaUiKsIhEbazMBhOHILES3EUYIGtpBwlDODhhFCTqQdtIYGf5H+eOVeFP4P3ul7L4fzHbZziQme9jDEqYhU8IkervbwjecYaRPziIeXuIuRNvESKRQjjRV+4f6XN8xQPkQ4wO0Ga9QPEeazKER4gi6uA3T9zy1KeCZb8McAY1RThCkpRtjAh2yJd/EuG0txPzy68BxT4RM2RS1FWEZH+IS1UUoRpuQ1JPw7sDfCDcnTx0rWrJ1pYSTckDwPuNgU/ADJ7ku1B60bXwAAAABJRU5ErkJggg==');
         }
+        @media (prefers-color-scheme: dark) {
+            ul li.dir-entry {
+                list-style-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAmklEQVR42mMYOuD////hQHwViK+g4SNALEeygVDDeLGIxwBxJzkGXsEh7gzEb0DyBPA1IC4BaRAGYh0gvgOiycAsUItdgHgKiDEdiPcC8Qoy8F0gtkY3cAYQ25ARj3C9VDJw1MBRA20pMNAWynaFGVgKxPVAzEi6gQi9QNwBxPkgQU4gng/N3FdIxDeA+AUQ3wbi2UDMzjDoAQBZ7/p98kNklQAAAABJRU5ErkJggg==');
+            }
+            ul li.file-entry{
+                list-style-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAwElEQVR42tzUAQbCUBzH8UebAsqadI0I1ICNAAl0haSDBIo2toMkUEARIBkF6QZdYX0R/ug9m0kMH/zhy+/hqSzLpD5CJAZLNKC+kUcdN4zgGbyw1UXl0cUBygQp5iJqDO5zBpWMlg0+sfg4YwerTHCKmfCAWzyod/xL0IaPQMOHVSToIESksUFTBCvwhh1ckWpc4FRrchsxEo0YrSJBC0N4GgPUfjX5BNf0wY4R5DTBHbYMSj2sEeW0wnt6yGYAAAiM90EeJ53sAAAAAElFTkSuQmCC');
+            }
+        }
 """
 
-markdown_css_link= """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css">"""
+markdown_css_link= """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.css">"""
 
 ### Main
 
